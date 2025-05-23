45a4350dce27ff7858d331855fd0c528d21b113f
luizzzmn <luizmanoel10098@gmail.com>
2025-05-23 11:32:23 -0300

    feat: footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // ← importa o hook
 import {getTopAnime} from '../API/getTopAnime.js'
 import './Home.css';
+import Footer from '../components/Footer';
 
 function TextoLimitado({ texto, limite = 20 }) {
   if (!texto) return null;
@@ -34,39 +35,56 @@ const Home = () => {
   };
 
   return (
-    <div className="scroll-highlight-container">
-      <h2 className="scroll-title">Em Destaque</h2>
-      {loading ? (
-        <p>Carregando...</p>
-      ) : (
-        <div className="scroll-wrapper">
-          <div className="anime-list">
-            {animes.map((anime) => (
-              <div
-                key={anime.mal_id}
-                className="anime-item"
-                onClick={() => handleAnimeClick(anime.mal_id)} // ← clique chama navegação
-                style={{ cursor: 'pointer' }} // ← estilo visual de botão
-              >
-                <img src={anime.images.jpg.image_url} alt={anime.title} />
-                <TextoLimitado texto={anime.title} limite={12}></TextoLimitado>
-                <p>Nota: {anime.score}/10</p>
-              </div>
-            ))}
-          </div>
+
+   <div className="page-container">
+    <main className="content">
+
+    
+
+      <div className="scroll-highlight-container">
+
+        <div className="destaques">
+          <h2>Animes em Destaque</h2>
+          <p>Lorem ipsum...</p>
         </div>
-      )}
-      <div className="extra-content">
-        <h2>Próximos Lançamentos</h2>
-        <p>Esse espaço pode ser usado para recomendações, resenhas ou outros conteúdos.</p>
-      </div>
 
-      <div className="extra-content">
-        <h2>Progresso Pessoal:</h2>
-        <p>Esse espaço pode ser usado para recomendações, resenhas ou outros conteúdos.</p>
-      </div>
+        <h2 className="scroll-title">Obras com maiores notas:</h2>
+        {loading ? (
+         <p>Carregando...</p>
+        ) : (
+          <div className="scroll-wrapper">
+            <div className="anime-list">
+              {animes.map((anime) => (
+                <div
+                  key={anime.mal_id}
+                  className="anime-item"
+                  onClick={() => handleAnimeClick(anime.mal_id)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <img src={anime.images.jpg.image_url} alt={anime.title} />
+                  <TextoLimitado texto={anime.title} limite={12}></TextoLimitado>
+                  <p>Nota: {anime.score}/10</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
 
-    </div>
+        <div className="proximos-lancamentos">
+          <h2>Próximos Lançamentos</h2>
+          <p>Lorem ipsum...</p>
+        </div>
+
+        <div className="progresso-pessoal">
+          <h2>Progresso Pessoal</h2>
+          <p>Lorem ipsum...</p>
+        </div>
+      </div>
+    </main>
+
+    <Footer />
+  </div>
+
   );
 };
 
